0a63c2241efdb9b6f3eab6b14b184183a939b5a8
driss <driss@LAPTOP-6QFS6SK0>
2022-11-10 21:20:17 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace EVALUATION
+{
+    
+    public class exam
+    {
+        int num_eval;
+        float duree_eval;
+        string matiere;
+        DateTime duree;
+        public class etudiant
+        {
+            string nom,prenom;
+            int num;
+
+        }   
+        
+        public class question
+        {
+            int num_quest;
+            string quest ;
+           
+            }
+
+        }
+    public class reponce:exam
+    {
+        string rep;
+        int num_quest;
+       
+    }
+    public class qsm:reponce
+    {
+        bool validite;
+        int num_quest;
+        bool est_valide();
+    }
+    public class rep_normale:reponce
+    {
+        string rep;
+        int point_quest;
+    }
+}     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
